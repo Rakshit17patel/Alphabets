c5f8a06e0b8d11350bb40582dfbfec051e3e6928
Rakshit17patel <65843585+Rakshit17patel@users.noreply.github.com>
2020-09-30 21:00:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+        var a = document.getElementById("myAudio"); 
+        var b = document.getElementById("myb"); 
+        var c = document.getElementById("c"); 
+        var d = document.getElementById("d"); 
+        var e = document.getElementById("e"); 
+        var f = document.getElementById("f"); 
+        var g = document.getElementById("g"); 
+        var h = document.getElementById("h"); 
+        var  i= document.getElementById("i"); 
+        var  j= document.getElementById("j"); 
+        var  k= document.getElementById("k"); 
+        var  l= document.getElementById("l"); 
+        var  m= document.getElementById("m"); 
+        var  n= document.getElementById("n"); 
+        var  o= document.getElementById("o"); 
+     //var  p= document.getElementById("p"); 
+        var  q= document.getElementById("q"); 
+        var  r= document.getElementById("r"); 
+        var  s= document.getElementById("s"); 
+        var  t= document.getElementById("t"); 
+        var  u= document.getElementById("u"); 
+        var  v= document.getElementById("v"); 
+        var  w= document.getElementById("w"); 
+        var  x= document.getElementById("x"); 
+        var  y= document.getElementById("y"); 
+        var  z= document.getElementById("z"); 
+        
+        function playAudio(p) { 
+            letter=p;
+          p.play(); 
+        } 
+         
+        
+         
+    
